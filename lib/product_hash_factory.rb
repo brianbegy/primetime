7aa9e5bb47c8f478a97dd4be6ef360d0966f99a8
Brian Begy <brian.begy@snapsheet.me>
2019-06-17 09:02:24 -0500

    update comment with the results of some cursory performance research

--- lib/product_hash_factory.rb
+++ lib/product_hash_factory.rb
@@ -8,11 +8,13 @@ module PrimeTime
     # The ON^2 aspect smells off.
     #
     # We could avoid double calculating the product by
-    # keeping a hash where the key is the two primes
-    # and the value the product,
-    # but I'm not convinced maintaining that is any faster than
-    # just doing the multiplication 2x
-    # building a times table *is* an On^2 problem.
+    # keeping a hash where the key is the first of the two primes,
+    # the value a hash with the key of the second and a value of the product.
+    # However, when I tested this,
+    # I found it to be about 2x slower that just multiplying
+    # for samples up to 100 x 100 integers.
+    #
+    # Building a times table *is* an On^2 problem.
     def self.create(input)
       data = {}
       input.each do |i|
